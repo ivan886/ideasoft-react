476399d71e00eeb39c0cdd38b4898c31ff1e83eb
ivan <ivanfernando.fonseca@gmail.com>
2020-08-18 09:36:17 -0500

    cambios en master

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hola master
+          Hola desde master
         </a>
       </header>
     </div>
